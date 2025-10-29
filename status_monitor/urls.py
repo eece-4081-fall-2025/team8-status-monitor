43e86878a5b62bed44b58aca0e126f8159819b3b
Rueben Murillo <ruemurillo@gmail.com>
2025-10-29 14:59:27 -0500

    added url patterns for authentication to status_monitor/urls.py

--- status_monitor/urls.py
+++ status_monitor/urls.py
@@ -16,9 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from .import views
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
+    path('register/', views.register, name='register'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
+    path('dashboard/', views.dashboard, name='dashboard'),
 ]
