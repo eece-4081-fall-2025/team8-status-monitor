dc00f3db4483963cda73638f7065839ed501a6b3
Rueben Murillo <ruemurillo@gmail.com>
2025-10-29 14:16:38 -0500

    added register view to views.py

--- status_monitor/views.py
+++ status_monitor/views.py
@@ -1,4 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login as auth_login, logout as auth_logout
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse
 
 def home(request):
     return render(request, 'home.html')
+
+
+#Begin user registration and authentication views
+def register(request):
+    if request.user.is_authenticated:
+        return redirect(reverse('dashboard'))
+    
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            auth_login(request, user)
+            return redirect(reverse('dashboard'))
+    else:
+        form = UserCreationForm()
+    return render(request, 'todo/register.html', {'form': form, 'title': 'Create Account'})
