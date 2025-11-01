72087d073c33bc644df5809be525080e2738dc98
Rueben Murillo <ruemurillo@gmail.com>
2025-11-01 09:59:53 -0500

    added setup to HomeViewTest since home page requires user login.

--- status_monitor/tests/test_dashboard.py
+++ status_monitor/tests/test_dashboard.py
@@ -1,9 +1,19 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.contrib.auth.models import User
 from django.urls import reverse
 
 
 class HomeViewTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.user = User.objects.create_user(
+            username='homeusertest',
+            password='HomePass123!'
+        )
+        self.client.login(username=self.user.username, password='HomePass123!')
+        self.home_url = reverse('home')
+        
     def test_home_page_loads_successfully(self):
-        response=self.client.get(reverse('home'))
+        response=self.client.get(self.home_url)
         self.assertEqual(response.status_code,200)
         self.assertContains(response, "Welcome to the status monitor")
