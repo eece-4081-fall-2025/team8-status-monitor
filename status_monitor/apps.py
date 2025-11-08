a96759e14b8aecb38d825b5265c60d4713251827
Rueben Murillo <ruemurillo@gmail.com>
2025-11-08 10:22:00 -0600

    setup for django_apscheduler

--- status_monitor/apps.py
+++ status_monitor/apps.py
@@ -1,7 +1,6 @@
 from django.apps import AppConfig
 
 class StatusMonitorConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
     name = 'status_monitor'
 
     def ready(self):
--- status_monitor/settings.py
+++ status_monitor/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'status_monitor'
+    'status_monitor',
+    'django_apscheduler',
 ]
 
 MIDDLEWARE = [
