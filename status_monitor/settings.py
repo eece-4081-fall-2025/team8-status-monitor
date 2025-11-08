11d6b90218e5d16c72a7dbda1db3190fd48ec07d
Rueben Murillo <ruemurillo@gmail.com>
2025-11-08 15:07:47 -0600

    Changes made to migrate to postgresql

--- status_monitor/settings.py
+++ status_monitor/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'status_monitor.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'status_monitor',
+        'USER': 'status_user',
+        'PASSWORD': 'changeme',  # match your psql setup
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
