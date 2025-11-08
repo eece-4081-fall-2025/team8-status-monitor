77a866bbfadcfc98c214292c3e8778591227088e
Rueben Murillo <ruemurillo@gmail.com>
2025-11-08 09:33:12 -0600

    Added Monitored_Site model to models.py

--- status_monitor/models.py
+++ status_monitor/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 class Site(models.Model):
     name = models.CharField(max_length=100)
     url = models.URLField(unique =True)
@@ -10,5 +10,14 @@ class Site(models.Model):
     #added dashboard fields
     status = models.CharField(max_length=10, choices=[('UP', 'UP'), ('DOWN', 'DOWN')], default='UP')
     response_ime = models.FloatField(default=0.0) #in seconds
+    def __str__(self):
+        return self.name
+    
+class MonitoredSite(models.Model):
+    name = models.CharField(max_length = 100)
+    url = models.URLField(unique = True)
+    check_frequency = models.IntegerField(help_text="Frequency (in minutes) to check site status")
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name = 'monitoreed_sites')
+    
     def __str__(self):
         return self.name
